d2927ea69eed1cb88fbceb83e0a3938e4acaa022
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-03-04 21:22:47 +0200

    minor changes to the initial setup

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,24 +1,21 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function BookItem({ book }) {
   return (
-
     <li>
-      <span>
-        {' '}
-        {book.name}
-        {' '}
-      </span>
-      <span>
-        {' '}
-        {book.author}
-        {' '}
-      </span>
-      <button type="button">Delete</button>
+      <h3>{book.category}</h3>
+      <h2>{book.name}</h2>
+      <h5>{book.author}</h5>
+      <div>
+        <button type="button">Remove</button>
+      </div>
     </li>
-
   );
 }
+
+BookItem.propTypes = {
+  book: PropTypes.arrayOf.isRequired,
+};
 
 export default BookItem;
