ef38efb4fb39b84e26178692059076f58400a83d
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-03-30 19:10:06 +0200

    fix bug

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import booksReducer from './books/books';
+import CategoriesReducer from './categories/categories';
 
 const reducer = combineReducers({
   books: booksReducer,
+  Categories: CategoriesReducer,
   // additional reducers could be added here
 });
 
